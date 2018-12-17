2546553e3bf1c4f3fc7c4e1844fbdbd90b549ebf
Zach <zachcanada23@gmail.com>
2018-12-17 13:07:25 -0600

    Drop down box for coordinates

--- FormIntoAnotherForm/FormIntoAnotherForm/Form1.cs
+++ FormIntoAnotherForm/FormIntoAnotherForm/Form1.cs
@@ -17,9 +17,12 @@ namespace FormIntoAnotherForm
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+      
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            
+
         }
+
     }
 }
